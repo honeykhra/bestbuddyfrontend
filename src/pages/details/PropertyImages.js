6fe2d47ff89f6b4f8391d4d7c013081b944330b3
honeykhra <honeykhra@gmail.com>
2023-04-03 18:56:10 +0530

    image carousel added

--- src/pages/details/PropertyImages.js
+++ src/pages/details/PropertyImages.js
@@ -1,18 +1,18 @@
 import React from "react";
-import "react-responsive-carousel/lib/styles/carousel.min.css";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from "react-responsive-carousel";
 
 const PropertyImages = ({ images = [] }) => {
-  console.log(images);
   return (
-    <section className="detail-banner-img">
-      <div className="banner-left">
-        <img src={images[0]} alt="banner-one" />
-      </div>
-      <div className="banner-right">
-        <img src={images[1]} alt="banner-two" />
-        <img src={images[2]} alt="banner-three" />
-      </div>
-    </section>
+    <>
+      <Carousel>
+        {images.map((image) => (
+          <div>
+            <img src={image} alt={image} />
+          </div>
+        ))}
+      </Carousel>
+    </>
   );
 };
 
