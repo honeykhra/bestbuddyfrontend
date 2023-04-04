f4497151bf084e91438dc3efab10082a0f87c03d
honeykhra <honeykhra@gmail.com>
2023-04-04 18:12:37 +0530

    preferences page styling fix

--- src/pages/preference/Preference.js
+++ src/pages/preference/Preference.js
@@ -4,16 +4,16 @@ import "./Preference.scss";
 import Footer from "../../components/Footer/Footer";
 import Navbar from "../../components/Navbar/Navbar";
 import PreferenceGrid from "../../components/PreferenceGrid/PreferenceGrid";
-import { Button } from 'react-bootstrap';
+import { Button } from "react-bootstrap";
 
 const Preference = () => {
   return (
     <div>
       <Navbar />
-      <div className="container content-container preferences-container">
-        <h3 className="preferences-heading">Preferences</h3>
+      <div className="preferences-container">
+        <h1 className="preferences-heading">Preferences</h1>
         <PreferenceGrid />
-        <Button className="submitbutton">Continue</Button>
+        <Button className="submitbutton">Update</Button>
       </div>
       <Footer />
     </div>
